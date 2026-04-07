05768d9c0c1990b95da7053293f98cfe08c0698b
Huxley Nyaoga <huxleynyaoga3@gmail.com>
2026-04-07 19:19:20 +0300

    db string changed OK

--- start-config-server.sh
+++ start-config-server.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 JAVA_EXEC="/usr/java/jdk-17/bin/java"
+
 export CONFIG_REPO_PATH="file:///u02/Domain/config-server/config-repo/"
 export CONFIG_USER="jubilee_admin"
 export CONFIG_PWD="secret123"
 export ENCRYPTION_KEY="jubilee-key-2024"
 export SERVER_PORT=8888
 
+export SPRING_CLOUD_CONFIG_SERVER_NATIVE_SEARCHLOCATIONS="$CONFIG_REPO_PATH"
+
 echo "=========================================="
-echo "JUBILEE CONFIG SERVER STARTUP"
+echo "JUBILEE CONFIG SERVER STARTUP (SECURE)"
 echo "REPO: $CONFIG_REPO_PATH"
 echo "PORT: $SERVER_PORT"
-echo "JAVA: $JAVA_EXEC"
 echo "=========================================="
 
 $JAVA_EXEC -jar target/config-server-0.0.1-SNAPSHOT.jar \
   --spring.profiles.active=native \
-  --spring.security.user.name="$CONFIG_USER" \
-  --spring.security.user.password="$CONFIG_PWD" \
   --encrypt.key="$ENCRYPTION_KEY" \
-  --spring.cloud.config.server.native.searchLocations="$CONFIG_REPO_PATH" \
   --server.port="$SERVER_PORT" \
   --logging.level.root=INFO \
   --logging.level.com.jubilee=DEBUG
